e1092d8ed024fed15ba9682e5aefeeb0f30d6c3c
manu-sj <manu.joseph@logicalclocks.com>
2025-02-28 19:08:59 +0100

    locust test changes

--- locust_benchmark/locustfile.py
+++ locust_benchmark/locustfile.py
@@ -1,3 +1,4 @@
+from gevent import monkey
 import random
 from common.hopsworks_client import HopsworksClient
 from common.stop_watch import stopwatch
@@ -5,6 +6,8 @@ from locust import HttpUser, User, task, constant, events
 from locust.runners import MasterRunner
 from urllib3 import PoolManager
 import nest_asyncio
+
+monkey.patch_all()
 
 
 @events.init.add_listener
@@ -65,8 +68,8 @@ class MySQLFeatureVectorLookup(User):
 
     def on_start(self):
         self.fv = self.client.get_or_create_fv()
-        self.fv.init_serving(external=self.client.external)
         nest_asyncio.apply()
+        self.fv.init_serving(external=self.client.external)
 
     @task
     def get_feature_vector(self):
