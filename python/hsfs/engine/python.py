0e152f39201009a1f9d3df357f6405ce8afc6ab3
manu-sj <manu.joseph@logicalclocks.com>
2025-05-16 12:39:59 +0200

    fixing logging feature vectors

--- python/hsfs/engine/python.py
+++ python/hsfs/engine/python.py
@@ -1935,12 +1935,13 @@ class Engine:
             extra_logging_features,
         ]:
             log_vectors: List[Dict[Any, str]] = []
+            print(f"{log_vectors=}, {data=}, {feature_names=}")
             # convert features to dict
             if data is None:
                 continue
             Engine._validate_logging_list(data, feature_names)
             if log_vectors is None:
-                log_vectors = [[dict(zip(feature_names, row)) for row in data]]
+                log_vectors = [dict(zip(feature_names, row)) for row in data]
             elif len(data) == 1 and len(log_vectors) > 1:
                 for log_vector in log_vectors:
                     log_vector.update(dict(zip(feature_names, data[0])))
